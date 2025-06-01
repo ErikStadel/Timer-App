1d2dd2138901aff7e7e9ba03d7819826e18ed746
ErikStadel <helmstroek@gmail.com>
2025-06-01 09:38:46 +0000

    Sound Pfade in sw.js

--- sw.js
+++ sw.js
@@ -2,14 +2,14 @@ self.addEventListener('install', event => {
     event.waitUntil(
         caches.open('timer-audio-cache-v1').then(cache => {
             return cache.addAll([
-                '/assets/sounds/bell.mp3',
-                '/assets/sounds/horn.mp3',
-                '/assets/sounds/beep.mp3',
-                '/assets/sounds/applause.mp3',
-                '/assets/sounds/chime.mp3',
-                '/assets/sounds/gong.mp3',
-                '/assets/sounds/whoosh.mp3',
-                '/assets/sounds/longbeep.mp3'
+                '/Timer-App/assets/sounds/bell.mp3',
+                '/Timer-App/assets/sounds/horn.mp3',
+                '/Timer-App/assets/sounds/beep.mp3',
+                '/Timer-App/assets/sounds/applause.mp3',
+                '/Timer-App/assets/sounds/chime.mp3',
+                '/Timer-App/assets/sounds/gong.mp3',
+                '/Timer-App/assets/sounds/whoosh.mp3',
+                '/Timer-App/assets/sounds/longbeep.mp3'
             ]);
         })
     );
